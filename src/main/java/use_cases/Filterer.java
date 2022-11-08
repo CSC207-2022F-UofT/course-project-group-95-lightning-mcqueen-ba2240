e2b88033e5d87c18ce0aa7cba90ce7be5e92d85c
john <jfitzgerald1126@gmail.com>
2022-11-08 18:09:57 -0500

    Changed the if condition to consider tags as a set rather than list and changes the tags implementation in timetable class

--- src/main/java/use_cases/Filterer.java
+++ src/main/java/use_cases/Filterer.java
@@ -1,16 +1,17 @@
 package use_cases;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 
 import entities.Timetable;
 import entities.Tags;
 
 public class Filterer {
-    public ArrayList<Timetable> filterAll(ArrayList<Timetable> timetable_list, Tags[] desired_tags) {
+    public ArrayList<Timetable> filterAll(ArrayList<Timetable> timetable_list, HashSet<String> desired_tags) {
         ArrayList<Timetable> filtered_timetables = new ArrayList<>();
 
         for (Timetable timetable: timetable_list) {
-            if (timetable.tags in desired_tags) {
+            if (timetable.getTags().containsAll(desired_tags)) {
                 filtered_timetables.add(timetable);
             }
         }
