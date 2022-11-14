7016675b05c25fd2d6cc604bbac9aa8df236b434
John Fitzgerald <j.fitzgerald@mail.utoronto.ca>
2022-11-14 17:38:07 -0500

    Fixed junit tests and added more documentation for Filterer

--- src/main/java/use_cases/Filterer.java
+++ src/main/java/use_cases/Filterer.java
@@ -5,12 +5,23 @@ import java.util.HashSet;
 
 import entities.Timetable;
 
+/**
+ * This class returns a sublist of the ArrayList containing all timetables that have all the tags in desired_tags.
+ */
 public class Filterer {
-    public ArrayList<Timetable> filterAll(ArrayList<Timetable> timetable_list, HashSet<String> desired_tags) {
-        ArrayList<Timetable> filtered_timetables = new ArrayList<>();
+    /**
+     * A static function to filter the timetables given desired_tags argument
+     * @param timetable_list the timetables to filter from
+     * @param desired_tags the tags in which the timetable_list will be filtered to
+     * @return a subarray of timetable_list such that every timetable's tags attribute is a superset of desired_tags
+     */
+    public static ArrayList<Timetable> filterAll(ArrayList<Timetable> timetable_list, HashSet<String> desired_tags) {
+        ArrayList<Timetable> filtered_timetables = new ArrayList<>(); // accumulator for filtered tables
 
         for (Timetable timetable: timetable_list) {
+            // check if all desired_tags present in the timetable's tags attribute
             if (timetable.getTags().containsAll(desired_tags)) {
+                // add the timetable to the accumulator
                 filtered_timetables.add(timetable);
             }
         }
