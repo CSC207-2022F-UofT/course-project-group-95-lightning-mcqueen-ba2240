7f0fe0a441f36e6d79aa916309ba20e0ec67a359
tuspar <tushar.rohith@gmail.com>
2022-12-05 22:28:15 -0500

    Warnings & Java Good Practises

--- src/main/java/controllers/AppController.java
+++ src/main/java/controllers/AppController.java
@@ -7,10 +7,10 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
-import javafx.scene.input.KeyEvent;
 import javafx.scene.text.Text;
 import org.controlsfx.control.textfield.TextFields;
 import presenters.ErrorWindow;
+import use_cases.auto_complete.AutoCompleteInputBoundary;
 import use_cases.auto_complete.AutoCompleteInteractor;
 import use_cases.auto_complete.AutoCompleteRequestModel;
 import use_cases.auto_complete.AutoCompleteResponseModel;
@@ -25,16 +25,11 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
 
-import static presenters.ErrorWindow.callError;
-
 /**
  * Controller for the main view
  */
 
 public class AppController implements Initializable {
-
-    @FXML
-    private Button generateButton;
 
     @FXML
     private TextField searchField;
@@ -48,18 +43,18 @@ public class AppController implements Initializable {
     @FXML
     private TextField yearField;
 
-    private final AutoCompleteInteractor autoCompleteInteractor = new AutoCompleteInteractor();
-
+    private final AutoCompleteInputBoundary autoCompleteInputBoundary = new AutoCompleteInteractor();
     private final TimetableGenerationInputBoundary timetableGenerationInputBoundary = new
             TimetableGenerationInteractor();
 
     private final List<String> courseList = new ArrayList<>();
-
     private List<Timetable> timetableList = new ArrayList<>();
 
-    // add course into courseList and input the new addition to be updated in the UI.
+    /**
+     * Add course into courseList and input the new addition to be updated in the UI.
+     */
     @FXML
-    void addCourse(ActionEvent event) {
+    void addCourse() {
         String course = searchField.getText();
         courseList.add(course);
         updateCourseListLabel();
@@ -67,18 +62,19 @@ public class AppController implements Initializable {
     }
 
     @FXML
-    void deleteCourse(ActionEvent event) {
-        if (courseList.size()>0) {
+    void deleteCourse() {
+        if (!courseList.isEmpty()) {
             courseList.remove(courseList.size() - 1);
             updateCourseListLabel();
         }
         else  {
-            callError("Insufficient number of courses.", "No courses have been added.");
+            ErrorWindow.callError("Insufficient number of courses.", "No courses have been added.");
         }
     }
 
-
-    // displays the user's selected courses in a label
+    /**
+     * Displays the user's selected courses in a label
+     */
     void updateCourseListLabel() {
         StringBuilder courseField = new StringBuilder("Added Courses:");
         for (String course: courseList) {
@@ -88,27 +84,25 @@ public class AppController implements Initializable {
     }
 
     @FXML
-    void generateCourses(ActionEvent event) {
+    void generateCourses() {
         TimetableGenerationRequestModel request = new TimetableGenerationRequestModel(courseList);
         TimetableGenerationResponseModel response = timetableGenerationInputBoundary.generate(request);
         timetableList = response.getTimetableList();
-
     }
 
     /**
      * SearchFieldTyped helps call the required functions to populate the search bar with appropriate suggestions
      * for auto complete to work properly, while also setting when the searchField can or cannot be edited.
-     * @param event parameter exists as a requirement for this function to be created but is not utilized.
      */
     @FXML
-    void searchFieldTyped(KeyEvent event) {
+    void searchFieldTyped() {
         new Thread(() -> {
             // Takes first 3 words from user input and uses that as org for the getSimpleCourses function.
             // Gets the keys from getSimpleCourses output and uses them as autocomplete suggestions.
             searchField.setEditable(false);
             AutoCompleteRequestModel request = new AutoCompleteRequestModel(searchField.getText(), yearField.getText(),
                     semesterField.getValue());
-            AutoCompleteResponseModel response = autoCompleteInteractor.search(request);
+            AutoCompleteResponseModel response = autoCompleteInputBoundary.search(request);
             TextFields.bindAutoCompletion(searchField, response.getCourses());
             searchField.setEditable(true);
         }).start();
