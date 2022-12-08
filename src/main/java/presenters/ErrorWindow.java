f82b6c33a4c1c17807d30fbcf12a810c35f76cef
tuspar <tushar.rohith@gmail.com>
2022-12-07 23:38:19 -0500

    ErrorWindow documentation

--- src/main/java/presenters/ErrorWindow.java
+++ src/main/java/presenters/ErrorWindow.java
@@ -4,6 +4,14 @@ import javafx.application.Platform;
 import javafx.scene.control.Alert;
 
 public class ErrorWindow {
+
+    private ErrorWindow() {}
+
+    /**
+     * Show an Error Window
+     * @param header Error header
+     * @param content Error message
+     */
     public static void callError(String header, String content) {
         Platform.runLater(() -> {
             Alert alert = new Alert(Alert.AlertType.ERROR);
