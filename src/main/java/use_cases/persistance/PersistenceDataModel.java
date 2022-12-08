f914b9fc9457f6cfe865858fb1046944ed9af378
tuspar <tushar.rohith@gmail.com>
2022-12-08 01:31:02 -0500

    Persistence Use Case Documentation

--- src/main/java/use_cases/persistance/PersistenceDataModel.java
+++ src/main/java/use_cases/persistance/PersistenceDataModel.java
@@ -4,17 +4,32 @@ import entities.base.Timetable;
 
 import java.util.List;
 
+/**
+ * The PersistenceDataModel class is a container for a list of Timetable objects.
+ */
 public class PersistenceDataModel {
     List<Timetable> timetables;
 
+    /**
+     * Constructs a PersistenceDataModel with a given list of Timetable objects.
+     * @param timetables the list of Timetable objects to be stored in this PersistenceDataModel
+     */
     public PersistenceDataModel(List<Timetable> timetables) {
         this.timetables = timetables;
     }
 
+    /**
+     * Returns the list of Timetable objects stored in this PersistenceDataModel.
+     * @return the list of Timetable objects stored in this PersistenceDataModel
+     */
     public List<Timetable> getTimetables() {
         return timetables;
     }
 
+    /**
+     * Sets the list of Timetable objects to be stored in this PersistenceDataModel.
+     * @param timetables the list of Timetable objects to be stored in this PersistenceDataModel
+     */
     public void setTimetables(List<Timetable> timetables) {
         this.timetables = timetables;
     }
